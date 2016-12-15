ba37e27c2c6d0f695d8f4a47b8055eb5716663fd
Majesty <a-o@qq.com>
2016-12-15 16:29:48 +0800

    修复了声调和空格功能

--- 拼音助手/拼音助手/Form1.Designer.cs
+++ 拼音助手/拼音助手/Form1.Designer.cs
@@ -30,14 +30,14 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_copy = new System.Windows.Forms.Button();
+            this.textBox_typein = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.checkBox_addspace = new System.Windows.Forms.CheckBox();
             this.checkBox_hasshengdiao = new System.Windows.Forms.CheckBox();
             this.textBox_input = new System.Windows.Forms.TextBox();
             this.textBox_output = new System.Windows.Forms.TextBox();
-            this.textBox_typein = new System.Windows.Forms.TextBox();
-            this.button_clear = new System.Windows.Forms.Button();
-            this.button_copy = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -67,6 +67,38 @@
             this.tabPage1.Text = "拼音输入";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(549, 250);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(75, 23);
+            this.button_clear.TabIndex = 8;
+            this.button_clear.Text = "清空";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // button_copy
+            // 
+            this.button_copy.Location = new System.Drawing.Point(468, 250);
+            this.button_copy.Name = "button_copy";
+            this.button_copy.Size = new System.Drawing.Size(75, 23);
+            this.button_copy.TabIndex = 7;
+            this.button_copy.Text = "复制";
+            this.button_copy.UseVisualStyleBackColor = true;
+            this.button_copy.Click += new System.EventHandler(this.button_copy_Click);
+            // 
+            // textBox_typein
+            // 
+            this.textBox_typein.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox_typein.Font = new System.Drawing.Font("新宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_typein.Location = new System.Drawing.Point(3, 3);
+            this.textBox_typein.Multiline = true;
+            this.textBox_typein.Name = "textBox_typein";
+            this.textBox_typein.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_typein.Size = new System.Drawing.Size(629, 241);
+            this.textBox_typein.TabIndex = 0;
+            this.textBox_typein.TextChanged += new System.EventHandler(this.textBox_typein_TextChanged);
+            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.checkBox_addspace);
@@ -92,6 +124,7 @@
             this.checkBox_addspace.TabIndex = 7;
             this.checkBox_addspace.Text = "字符间空格";
             this.checkBox_addspace.UseVisualStyleBackColor = true;
+            this.checkBox_addspace.CheckedChanged += new System.EventHandler(this.checkBox_addspace_CheckedChanged);
             // 
             // checkBox_hasshengdiao
             // 
@@ -104,6 +137,7 @@
             this.checkBox_hasshengdiao.TabIndex = 4;
             this.checkBox_hasshengdiao.Text = "带声调";
             this.checkBox_hasshengdiao.UseVisualStyleBackColor = true;
+            this.checkBox_hasshengdiao.CheckedChanged += new System.EventHandler(this.checkBox_hasshengdiao_CheckedChanged);
             // 
             // textBox_input
             // 
@@ -118,6 +152,7 @@
             // textBox_output
             // 
             this.textBox_output.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox_output.Font = new System.Drawing.Font("新宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.textBox_output.Location = new System.Drawing.Point(3, 3);
             this.textBox_output.Multiline = true;
             this.textBox_output.Name = "textBox_output";
@@ -125,38 +160,6 @@
             this.textBox_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox_output.Size = new System.Drawing.Size(629, 117);
             this.textBox_output.TabIndex = 8;
-            // 
-            // textBox_typein
-            // 
-            this.textBox_typein.Dock = System.Windows.Forms.DockStyle.Top;
-            this.textBox_typein.Font = new System.Drawing.Font("新宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox_typein.Location = new System.Drawing.Point(3, 3);
-            this.textBox_typein.Multiline = true;
-            this.textBox_typein.Name = "textBox_typein";
-            this.textBox_typein.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox_typein.Size = new System.Drawing.Size(629, 241);
-            this.textBox_typein.TabIndex = 0;
-            this.textBox_typein.TextChanged += new System.EventHandler(this.textBox_typein_TextChanged);
-            // 
-            // button_clear
-            // 
-            this.button_clear.Location = new System.Drawing.Point(549, 250);
-            this.button_clear.Name = "button_clear";
-            this.button_clear.Size = new System.Drawing.Size(75, 23);
-            this.button_clear.TabIndex = 8;
-            this.button_clear.Text = "清空";
-            this.button_clear.UseVisualStyleBackColor = true;
-            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
-            // 
-            // button_copy
-            // 
-            this.button_copy.Location = new System.Drawing.Point(468, 250);
-            this.button_copy.Name = "button_copy";
-            this.button_copy.Size = new System.Drawing.Size(75, 23);
-            this.button_copy.TabIndex = 7;
-            this.button_copy.Text = "复制";
-            this.button_copy.UseVisualStyleBackColor = true;
-            this.button_copy.Click += new System.EventHandler(this.button_copy_Click);
             // 
             // Form1
             // 
@@ -166,7 +169,8 @@
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "拼音助手";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
